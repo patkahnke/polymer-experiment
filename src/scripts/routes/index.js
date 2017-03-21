d1b493bf7d42f56903525fa3a4b7fc9089f2d345
Patrick Kahnke <patrickkahnke@Patricks-MacBook-Pro.local>
2017-03-20 19:58:56 -0500

    cleanup

--- src/scripts/routes/index.js
+++ src/scripts/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-// Handle my-app file separately
+// Handle index file separately
 // Also catches any other request not explicitly matched elsewhere
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../../../index.html'));
